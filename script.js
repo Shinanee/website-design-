852009e1f9fdea3c9fdd464d8007f981bc5c9be6
Shivani Rathaur <designwebsite467@gmail.com>
2025-11-14 11:00:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,19 +31,7 @@ function showTool(tool) {
         </div>`;
       break;
 
-    case "quotation":
-      area.innerHTML = `
-        <div class='tool-box'>
-          <h3>Quotation Approval (AI)</h3>
-          <label>Total Order Value (₹):</label>
-          <input type='number' id='qValue'>
-          <label>Discount (%):</label>
-          <input type='number' id='qDisc'>
-          <button onclick='approveQuote()'>Check AI Approval</button>
-          <div class='result' id='quoteResult'></div>
-        </div>`;
-      break;
-
+   
     case "payment":
       area.innerHTML = `
         <div class='tool-box'>
@@ -100,3 +88,4 @@ function submitPayment() {
 
 // Load default tool
 showTool("discount");
+
